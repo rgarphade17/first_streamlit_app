97016102add427f0d0b6885ab60e204eb69ffc23
Rupesh Vinayak Garphade <105192159+rgarphade17@users.noreply.github.com>
2022-05-09 11:59:15 +0530

    Update streamlit_app.py
    updated moms

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
-streamlit.title('My parents new healthy diner')
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & blueberry otmeals')
-streamlit.text('Kale, spinech & Rocket Smothie')
-streamlit.text('Hard Boiled free-range eggs')
+streamlit.title('My MOM\'s new healthy diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
